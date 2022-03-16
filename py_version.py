95ab4c9d5a6f7b34ea3c96f7359f8f8b984f8c15
Jefferson Boothe <46006304+shamuraii@users.noreply.github.com>
2022-03-16 01:43:10 -0400

    First Release
    Added optional debug flag
    Added graph outputs and more csv output files
    Added more command-line info while running
    Fixed the end-of-file video processing results

--- py_version.py
+++ py_version.py
@@ -1,16 +1,19 @@
 from imutils.video import FileVideoStream
 from imutils.video import FPS
+from pathlib import Path
 import numpy as np
 import argparse
 import cv2
 import imutils
-import time
+import math
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import time
 
 # Parameters
 SAFE_THRESH = 60
 MIN_THRESH = 25
-FRAMES_PER_SLIT = 2000
+FRAMES_PER_SLIT = 4000
 
 # Globals
 FRAMES_PER_SEC = 0
@@ -19,15 +22,21 @@ SLIT_POS = 0
 FRAME_COUNT = 0
 
 def filterFrame(frame):
-    fgray = frame[:,:,2]
-    slit = fgray[:,SLIT_POS]
+    try:
+        fgray = frame[:,:,2]
+        slit = fgray[:,SLIT_POS]
+    except TypeError:
+        print("[INFO] End of file reached...")
+        return None
     return slit
 
-# speed up data reading
-# https://github.com/PyImageSearch/imutils/blob/master/demos/read_frames_fast.py
+# Get video file path from commandline args
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required=True, help="path to input video file")
+ap.add_argument("-d", "--debug", required=False, help="enable debug windows", default=False, type=bool)
+#ap.add_argument("-o", "--output", required=False, help="Output file name as CSV (Ex: \"data_out.csv\"")
 args = vars(ap.parse_args())
+DEBUG_MODE = args["debug"]
 
 print("Gathering Video Information...")
 cap = cv2.VideoCapture(args["video"])
@@ -60,12 +69,27 @@ i = 0
 loop_count = 0
 while fvs.running():
     slit = fvs.read()
+    if slit is None:
+        fvs.stop()
+        break
     fps.update()
     slits_im[:,i] = slit
     fnum = loop_count * FRAMES_PER_SLIT + i
     raw_data[fnum][0] = fnum
     i += 1
+
+    slit_length = 0
+    run_flag = 0
+    if (fnum == FRAME_COUNT - 1):
+        true_count = int(FRAME_COUNT % FRAMES_PER_SLIT)
+        slits_im = np.delete(slits_im, slice(int(true_count),int(FRAMES_PER_SLIT-1)), 1)
+        slit_length = true_count
+        run_flag = 2
     if (i == FRAMES_PER_SLIT):
+        slit_length = FRAMES_PER_SLIT
+        run_flag = 1
+    
+    if (run_flag != 0):
         #plt.hist(slits_im.ravel(),256,[0,256])
         #plt.show()
         #cv2.waitKey(0)
@@ -74,7 +98,7 @@ while fvs.running():
         slit_f = cv2.GaussianBlur(slits_im, (5,5), 0)
         mean_col = np.mean(slit_f, axis=1)
         col_image = slit_f.copy()
-        for i in range(FRAMES_PER_SLIT):
+        for i in range(int(slit_length)):
             col_image[:,i] = mean_col
         # Subtract Background + Normalize from 0-255
         slit_front = cv2.subtract(slit_f, col_image)
@@ -82,7 +106,7 @@ while fvs.running():
         norm_front_f = cv2.GaussianBlur(norm_front, (5,5), 0)
         # Get OTSU Threshold
         T, otsu_f = cv2.threshold(norm_front_f, 0, 255, cv2.THRESH_OTSU)
-        print("[INFO] Thresh = %s" % T)
+        print("[INFO] Loop Count: %d\tThresh = %d\tFrame Num: %d" % (loop_count,T,fnum))
         # Increase Threshold if below minimum
         if (T < MIN_THRESH): T = SAFE_THRESH
         thresh_f = cv2.inRange(norm_front_f, T, 255)
@@ -108,24 +132,79 @@ while fvs.running():
             else:
                 cX, cY = 0, 0
 
-        cv2.imshow("Original", og_copy)
-        cv2.imshow("Front", norm_front)
-        cv2.imshow("open_f", open_f)
-        cv2.waitKey(0)
+        if (DEBUG_MODE):
+            cv2.imshow("Original", og_copy)
+            cv2.imshow("Front", norm_front)
+            cv2.imshow("open_f", open_f)
+            cv2.waitKey(0)
 
         # Reset variables for next batch
         slits_im.fill(0)
         i = 0
         loop_count += 1
 
+        if (run_flag == 2):
+            fvs.stop()
+
 # stop the timer and display FPS information
 fps.stop()
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-np.savetxt("raw.csv", raw_data, delimiter=",", fmt="%d")
+# Get Sec and Total data
+total_cells = 0
+total_sec = math.ceil(FRAME_COUNT / FRAMES_PER_SEC)
+sec_data = np.zeros((int(total_sec),2), np.uint16)
+tot_data = np.zeros((int(total_sec),2), np.uint16)
+for row in raw_data:
+    sec_data[math.floor(row[0] / FRAMES_PER_SEC)][1] += row[1]
+    total_cells += row[1]
+for i in range(int(total_sec)):
+    sec_data[i][0] = i
+    tot_data[i][0] = i
+    if (i == 0):
+        tot_data[i][1] = sec_data[i][1]
+    else:
+        tot_data[i][1] = sec_data[i][1] + tot_data[i-1][1]
+print("[INFO] Total cells: %d" % total_cells)
 
-cv2.waitKey(0)
+# Save Raw data and Sec data to csv files
+out_raw = Path(args["video"]).stem + "_raw"
+out_sec = Path(args["video"]).stem + "_sec"
+out_tot = Path(args["video"]).stem + "_tot"
+np.savetxt(out_raw + ".csv", raw_data, delimiter=",", fmt="%d")
+np.savetxt(out_sec + ".csv", sec_data, delimiter=",", fmt="%d")
+np.savetxt(out_tot + ".csv", sec_data, delimiter=",", fmt="%d")
+print("[INFO] Raw data saved to: %s" % (out_raw + ".csv"))
+print("[INFO] Per Second data saved to: %s" % (out_sec + ".csv"))
+print("[INFO] Cumulative Totals saved to: %s" % (out_tot + ".csv"))
+
+# Save graph outputs of raw data and sec data
+x,y = raw_data.T
+plt.plot(x,y)
+plt.title("Cells per Frame")
+plt.xlabel("Frame Number")
+plt.ylabel("Cells Counted")
+plt.savefig((out_raw + ".png"), bbox_inches='tight')
+
+plt.close()
+x,y = sec_data.T
+plt.plot(x,y)
+plt.title("Cells per Second")
+plt.xlabel("Time (t)")
+plt.ylabel("Cells Counted")
+plt.savefig((out_sec + ".png"), bbox_inches='tight')
+
+plt.close()
+x,y = tot_data.T
+plt.plot(x,y)
+plt.title("Cumulative Cells per Second")
+plt.xlabel("Time (t)")
+plt.ylabel("Total Cells Counted")
+plt.savefig((out_tot + ".png"), bbox_inches='tight')
+
+if (DEBUG_MODE):
+    cv2.waitKey(0)
 cv2.destroyAllWindows()
 fvs.stop()
 quit()
